ba645a1d40259e87e4323c2acbf7d3a447b84fd8
Siroshun09 <siro.shun8@gmail.com>
2025-09-21 16:09:20 +0900

    feat: Placeholder now overrides Function#compose

--- src/main/java/dev/siroshun/mcmsgdef/Placeholder.java
+++ src/main/java/dev/siroshun/mcmsgdef/Placeholder.java
@@ -1,7 +1,9 @@
 package dev.siroshun.mcmsgdef;
 
 import net.kyori.adventure.text.ComponentLike;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
 import java.util.function.Function;
 
 /**
@@ -10,4 +12,11 @@ import java.util.function.Function;
  * @param <T> the type of the source object
  */
 public interface Placeholder<T> extends Function<T, ComponentLike> {
+
+    @Override
+    default <V> @NotNull Placeholder<V> compose(@NotNull Function<? super V, ? extends T> before) {
+        Objects.requireNonNull(before);
+        return (V v) -> this.apply(before.apply(v));
+    }
+
 }
